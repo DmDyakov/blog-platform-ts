2ec571249aa55828e114dcf915a764f35268c3f4
Dmitry Dyakov <DmDyakov@users.noreply.github.com>
2021-10-14 15:33:30 +0300

    add routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,8 @@ import ArticleEditingPage from 'src/pages/article-editing-page';
 import ArticleListPage from 'src/pages/article-list-page';
 import ArticleSinglePage from 'src/pages/article-single-page';
 import AuthPage from 'src/pages/auth-page';
+import { ArticleFormType } from 'src/typings/articleForm';
+import { FormType } from 'src/typings/authForm';
 
 import classes from './app.module.scss';
 
@@ -15,12 +17,19 @@ const App = () => {
         <AppHeader />
         <div className={classes.page}>
           <Switch>
-            <Route exact path="/auth/:formType" component={AuthPage} />
+            <Route
+              exact
+              path={`/auth/:formType(${FormType.SIGN_IN}|${FormType.SIGN_UP}|${FormType.EDIT_PROFILE})`}
+              component={AuthPage}
+            />
             <Route exact path="/articles/page/:pageNumber" component={ArticleListPage} />
             <Route exact path="/article/:slug" component={ArticleSinglePage} />
-            <Route exact path="/article/edit/:slug" component={ArticleEditingPage} />
-            <Route exact path="/article/edit/new" component={ArticleEditingPage} />
-            <Redirect to="/article/edit/new" />
+            <Route
+              exact
+              path={`/article/editing/:formType(${ArticleFormType.CREATE}|${ArticleFormType.EDIT})`}
+              component={ArticleEditingPage}
+            />
+            <Redirect to={`/articles/page/1`} />
           </Switch>
         </div>
       </Router>
